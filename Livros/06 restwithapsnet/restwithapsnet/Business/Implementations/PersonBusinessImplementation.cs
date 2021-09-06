5c9132a6f6dc3436cf92c567012b9bc0dc050062
felipegith <felipefknowx@gmail.com>
2021-09-06 18:36:42 -0300

    Implementando o padrão do projeto: Generic Repository

---
+++ Livros/06 restwithapsnet/restwithapsnet/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,49 @@
+﻿using restwithapsnet.Model;
+using restwithapsnet.Repository;
+using System.Collections.Generic;
+
+
+
+
+namespace restwithapsnet.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public Person Create(Person person)
+        {
+
+            return _repository.Create(person);
+        }
+
+        public Person Update(Person person)
+        {
+
+
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+    }
+}
---
+++ Livros/06 restwithapsnet/restwithapsnet/Controllers/BookController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using restwithapsnet.Business;
+using restwithapsnet.Model;
+
+
+namespace restwithapsnet.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        private readonly ILogger<BookController> _logger;
+        private IBookBusiness _bookBusiness;
+        public BookController(ILogger<BookController> logger, IBookBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_bookBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _bookBusiness.FindById(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Book book)
+        {
+
+
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_bookBusiness.Create(book));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Book book)
+        {
+
+
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_bookBusiness.Update(book));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+
+            _bookBusiness.Delete(id);
+            return NoContent();
+
+        }
+    }
+}
