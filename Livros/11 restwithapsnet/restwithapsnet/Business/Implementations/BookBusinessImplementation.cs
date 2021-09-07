db4581e01f8db78ec48abd10ff504e9c968302ba
felipegith <felipefknowx@gmail.com>
2021-09-07 12:34:21 -0300

    Até aqui nós SÓ configuramos nosso código para que possamos implementar o HATEOS. A implementação foi do 1 até o 4

---
+++ Livros/11 restwithapsnet/restwithapsnet/Business/Implementations/BookBusinessImplementation.cs
@@ -0,0 +1,49 @@
+﻿using restwithapsnet.Data.Converter.Implementations;
+using restwithapsnet.Model;
+using restwithapsnet.Repository;
+using System.Collections.Generic;
+
+
+namespace restwithapsnet.Business.Implementations
+{
+    public class BookBusinessImplementation : IBookBusiness
+    {
+        private readonly IRepository<Book> _repository;
+
+        private readonly BookConverter _converter;
+
+        public BookBusinessImplementation(IRepository<Book> repository)
+        {
+            _repository = repository;
+            _converter = new BookConverter();
+        }
+        public BookVO Create(BookVO book)
+        {
+            var personEntity = _converter.Parse(book);
+            personEntity = _repository.Create(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<BookVO> FindAll()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        public BookVO FindById(long id)
+        {
+            return _converter.Parse(_repository.FindById(id));
+        }
+
+        public BookVO Update(BookVO book)
+        {
+            var personEntity = _converter.Parse(book);
+            personEntity = _repository.Update(personEntity);
+            return _converter.Parse(personEntity);
+        }
+    }
+}
---
+++ Livros/11 restwithapsnet/restwithapsnet/Hypermedia/Abstract/ISupportHyperMedia.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+
+namespace restwithapsnet.Hypermedia.Abstract
+{
+    public interface ISupportHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
