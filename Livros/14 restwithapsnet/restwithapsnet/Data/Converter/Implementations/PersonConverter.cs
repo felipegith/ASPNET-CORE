f1be64e913b97c52eb93be9223d424ae1916b2d6
felipegith <felipefknowx@gmail.com>
2021-09-07 18:14:50 -0300

    Customizando o Swagger

---
+++ Livros/14 restwithapsnet/restwithapsnet/Data/Converter/Implementations/PersonConverter.cs
@@ -0,0 +1,73 @@
+﻿using restwithapsnet.Data.Converter.Contract;
+using restwithapsnet.Data.VO;
+using restwithapsnet.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace restwithapsnet.Data.Converter.Implementations
+{
+    public class PersonConverter : IParser<PersonVO, Person>, IParser<Person, PersonVO>
+    {
+        public Person Parse(PersonVO origem)
+        {
+            if (origem == null)
+            {
+                return null;
+            }
+            else
+            {
+                return new Person
+                {
+                    Id = origem.Id,
+                    FirstName = origem.FirstName,
+                    LastName = origem.LastName,
+                    Address = origem.Address,
+                    Gender = origem.Gender
+                };
+            }
+        }
+
+        public List<Person> Parse(List<PersonVO> origem)
+        {
+            if (origem == null)
+            {
+                return null;
+            }
+            else
+            {
+                return origem.Select(item => Parse(item)).ToList();
+            }
+        }
+
+        public PersonVO Parse(Person origem)
+        {
+            if (origem == null)
+            {
+                return null;
+            }
+            else
+            {
+                return new PersonVO
+                {
+                    Id = origem.Id,
+                    FirstName = origem.FirstName,
+                    LastName = origem.LastName,
+                    Address = origem.Address,
+                    Gender = origem.Gender
+                };
+            }
+        }
+
+        public List<PersonVO> Parse(List<Person> origem)
+        {
+            if (origem == null)
+            {
+                return null;
+            }
+            else
+            {
+                return origem.Select(item => Parse(item)).ToList();
+            }
+        }
+    }
+}
