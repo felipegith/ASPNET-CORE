160b31417d2ba067b75cc27beefc3eaaca712298
felipegith <felipefknowx@gmail.com>
2021-09-07 12:10:43 -0300

    Implementando HATEOS 3

---
+++ Livros/10 restwithapsnet/restwithapsnet/Data/VO/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace restwithapsnet.Model
+{
+    
+    public class BookVO 
+    {
+
+        public long Id { get; set; }
+
+        public string Author { get; set; }
+               
+        public string Title { get; set; }
+                
+        public decimal Price { get; set; }
+                
+        public DateTime LaunchDate { get; set; }
+    }
+}
---
+++ Livros/10 restwithapsnet/restwithapsnet/Hypermedia/HyperMediaLink.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restwithapsnet.Hypermedia
+{
+    public class HyperMediaLink
+    {
+        public string Real { get; set; }
+        public string Href
+        {
+            get
+            {
+                object _look = new object();
+                lock (_look)
+                {
+                    StringBuilder sb = new StringBuilder(href);
+                    return sb.Replace("%2F", "/").ToString();
+                }
+            }
+            set
+            {
+                href = value;
+            }
+        }
+
+
+        private string href; // Atributo para manipualr a // no endereço 
+        public string Type { get; set; }
+        public string Action { get; set; }
+    }
+}
