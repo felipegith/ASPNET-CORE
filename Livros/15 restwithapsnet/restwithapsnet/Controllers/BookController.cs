94dd8c944ec42837b8289c3a681fba57dbf7e731
felipegith <felipefknowx@gmail.com>
2021-09-07 21:06:44 -0300

    Adicionando CORS na aplicação

---
+++ Livros/15 restwithapsnet/restwithapsnet/Controllers/BookController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using restwithapsnet.Business;
+using restwithapsnet.Hypermedia.Filters;
+using restwithapsnet.Model;
+using System.Collections.Generic;
+
+namespace restwithapsnet.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        private readonly ILogger<BookController> _logger;
+        private IBookBusiness _bookBusiness;
+        public BookController(ILogger<BookController> logger, IBookBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        [ProducesResponseType((200), Type = typeof(List<BookVO>))]
+        [ProducesResponseType((204))]
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_bookBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType((200), Type = typeof(BookVO))]
+        [ProducesResponseType((204))]
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var person = _bookBusiness.FindById(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        [ProducesResponseType((200), Type = typeof(BookVO))]
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] BookVO book)
+        {
+
+
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_bookBusiness.Create(book));
+        }
+
+        [HttpPut]
+        [ProducesResponseType((200), Type = typeof(BookVO))]
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] BookVO book)
+        {
+
+
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_bookBusiness.Update(book));
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType((204))]
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        public IActionResult Delete(long id)
+        {
+
+            _bookBusiness.Delete(id);
+            return NoContent();
+
+        }
+    }
+}
---
+++ Livros/15 restwithapsnet/restwithapsnet/Data/VO/PersonVO.cs
@@ -0,0 +1,22 @@
+﻿
+using restwithapsnet.Hypermedia;
+using restwithapsnet.Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace restwithapsnet.Data.VO
+{
+
+    public class PersonVO : ISupportHyperMedia
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
