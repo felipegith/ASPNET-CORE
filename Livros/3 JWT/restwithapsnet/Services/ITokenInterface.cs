7b6e35f04468a8a4198bf10dbfd662cc282e6c8e
felipegith <felipefknowx@gmail.com>
2021-09-08 09:37:23 -0300

    Implementando método responsável por atualizar as informações do usuário

---
+++ Livros/3 JWT/restwithapsnet/Services/ITokenInterface.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace restwithapsnet.Services
+{
+    public interface ITokenInterface
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
