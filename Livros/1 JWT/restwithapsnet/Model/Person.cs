200b9f8cfa41927a90b6ececa60a6b8d724833d1
felipegith <felipefknowx@gmail.com>
2021-09-07 21:40:21 -0300

    1 - JWT. Criando a tabela no banco de dados

---
+++ Livros/1 JWT/restwithapsnet/Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using restwithapsnet.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace restwithapsnet.Model
+{
+    [Table("person")]
+    public class Person : BaseEntity
+    {
+        
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("address")]
+        public string Address { get; set; }
+
+        [Column("gender")]
+        public string Gender { get; set; }
+    }
+}
